d44e9cef70969183564d59c7494b288268ce11c6
Santiag077127 <santiagogordoperez77@gmail.com>
2026-03-27 22:29:37 -0500

    correcion

--- 01_tables/02-create-book.sql
+++ 01_tables/02-create-book.sql
@@ -1,18 +1,3 @@
-CREATE DATABASE biblioteca;
-
-CREATE TABLE clientes (
-	id_cliente int,
-	cliente_nombre varchar (50),
-	correo varchar (100),
-	cuidad varchar (50),
-	created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ,
-    deleted_at TIMESTAMPTZ,
-    created_by UUID,
-    updated_by UUID,
-    deleted_by UUID,
-    status BOOLEAN DEFAULT TRUE
-)
 
 CREATE TABLE libros (
 	id_libro int,
